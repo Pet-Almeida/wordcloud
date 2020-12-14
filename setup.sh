7b8c276435dfc41b730c2612f67848c97aa17df3
Pet-Almeida <76003826+Pet-Almeida@users.noreply.github.com>
2020-12-14 20:52:03 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"peter.souza.a@hotmail.com\"\n\
-" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+\n\
 " > ~/.streamlit/config.toml
